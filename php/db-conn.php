a858107b6137edc9591e1c1e46c7c6d11e2f1d03
Kosta Jovanovic <kosta.jovanovic86@gmail.com>
2019-08-17 17:52:24 +0200

    prpravljeno za MagmalKviz bazu

--- php/db-conn.php
+++ php/db-conn.php
@@ -1,11 +1,11 @@
 <?php
 
 $host = 'localhost';
-$dbname = 'magnall';
+$dbname = 'MagnalKviz';
 $charset = 'utf8mb4';
 
-$username = 'root';
-$password = 'password';
+$username = 'kosta';
+$password = 'kosta1986';
 $options = [
 			PDO::ATTR_ERRMODE 			 => PDO::ERRMODE_EXCEPTION,
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
--- php/user_input.php
+++ php/user_input.php
@@ -9,15 +9,14 @@ $godine = $recived_array[1];
 $fizicka_aktivnost = $recived_array[2];
 $san = $recived_array[3];
 $pritisak = $recived_array[4];
-$checked_values = $recived_array["checkedValuesStr"];
+$checked_answers = $recived_array["checkedValuesStr"];
 
 //PDO prepared statement to send to database
 try {
 
     $pdo = new PDO($dsn, $username, $password, $options);
 
-    $query = "INSERT INTO user_input(Pol,Godine,Fizicka_aktivnost,Kakav_vam_je_san,Kakav_vam_je_krvni_pritisak,checked_answers) 
-    VALUES(:pol,:godine,:fizicka_aktivnost,:san,:pritisak,:checked_values)";
+    $query = "INSERT INTO magnal_kviz_odgovori(Pol,Godine,Fizicka_aktivnost,Kakav_vam_je_san,Kakav_vam_je_krvni_pritisak,checked_answers) VALUES(:pol,:godine,:fizicka_aktivnost,:san,:pritisak,:checked_answers)";
 
     $statement = $pdo->prepare($query);
 
@@ -26,7 +25,7 @@ try {
     $statement->bindParam(':fizicka_aktivnost', $fizicka_aktivnost, PDO::PARAM_STR);
     $statement->bindParam(':san', $san, PDO::PARAM_STR);
     $statement->bindParam(':pritisak', $pritisak, PDO::PARAM_STR);
-    $statement->bindParam(':checked_values', $checked_values, PDO::PARAM_STR);
+    $statement->bindParam(':checked_answers', $checked_answers, PDO::PARAM_STR);
 
     $statement->execute();
 
